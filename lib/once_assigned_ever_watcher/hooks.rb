ab5010439a26048c05c026203650d0c221cdd849
James Moore <james@panic.com>
2015-03-16 13:01:39 -0700

    add anyone who makes changes as a watcher

--- lib/once_assigned_ever_watcher/hooks.rb
+++ lib/once_assigned_ever_watcher/hooks.rb
@@ -4,6 +4,9 @@ module OnceAssignedEverWatcher
     def controller_issues_edit_before_save (context={})
       if context[:issue] && context[:issue].id != nil                      
         issue = Issue.find_by_id(context[:issue].id)      
+        Watcher.find_or_create_by_watchable_type_and_watchable_id_and_user_id("Issue", 
+                                                                              context[:issue].id, 
+                                                                              User.current.id)
         if issue.assigned_to_id != nil
           Watcher.find_or_create_by_watchable_type_and_watchable_id_and_user_id("Issue", 
                                                                                 context[:issue].id, 
