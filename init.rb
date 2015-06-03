3ad169740828f5da3549c5c971a2ac4daea84be2
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2015-06-03 10:35:24 +0200

    Compatible to Redmine 3

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require_dependency 'once_assigned_ever_watcher/hooks'
 Redmine::Plugin.register :once_assigned_ever_watcher do
   name 'Once Assigned Ever Watcher plugin'
   author 'Kevin Neuenfeldt'
-  description 'If you change the assignee of an issue he will automatically become a watcher of that issue'
+  description 'If you change the assignee of an issue he will automatically become a watcher of that issue.'
   version '0.1.0'
   url 'www.enervision.eu'
-  author_url 'www.thetalogn.de'
+  author_url ''
 end
--- lib/once_assigned_ever_watcher/hooks.rb
+++ lib/once_assigned_ever_watcher/hooks.rb
@@ -3,11 +3,11 @@ module OnceAssignedEverWatcher
   
     def controller_issues_edit_before_save (context={})
       if context[:issue] && context[:issue].id != nil                      
-        issue = Issue.find_by_id(context[:issue].id)      
+        issue = Issue.find(context[:issue].id)
         if issue.assigned_to_id != nil
-          Watcher.find_or_create_by_watchable_type_and_watchable_id_and_user_id("Issue", 
-                                                                                context[:issue].id, 
-                                                                                issue.assigned_to_id)
+          Watcher.find_or_create_by(watchable_type: "Issue",
+                                    watchable_id: context[:issue].id,
+                                    user_id: issue.assigned_to_id)
         end
       end
     end
