7a9c28f229e7605f501929d5f344bc434ec9a706
James Moore <hello@jmoore.me>
2016-05-31 18:13:34 -0700

    bad merge

--- lib/once_assigned_ever_watcher/hooks.rb
+++ lib/once_assigned_ever_watcher/hooks.rb
@@ -4,9 +4,6 @@ module OnceAssignedEverWatcher
     def controller_issues_edit_before_save (context={})
       if context[:issue] && context[:issue].id != nil                      
         issue = Issue.find(context[:issue].id)
-        Watcher.find_or_create_by_watchable_type_and_watchable_id_and_user_id("Issue", 
-                                                                              context[:issue].id, 
-                                                                              User.current.id)
         if issue.assigned_to_id != nil
           Watcher.find_or_create_by(watchable_type: "Issue",
                                     watchable_id: context[:issue].id,
