267ac4cdd58da0ad49d191c74cd7946bbd3afc53
James Moore <hello@jmoore.me>
2017-02-22 15:37:19 -0800

    adds a watcher whenever someone comments

--- lib/once_assigned_ever_watcher/hooks.rb
+++ lib/once_assigned_ever_watcher/hooks.rb
@@ -1,16 +1,22 @@
 module OnceAssignedEverWatcher
   class Hooks < Redmine::Hook::ViewListener
-  
+
     def controller_issues_edit_before_save (context={})
-      if context[:issue] && context[:issue].id != nil                      
-        issue = Issue.find(context[:issue].id)
-        if issue.assigned_to_id != nil
-          Watcher.find_or_create_by(watchable_type: "Issue",
-                                    watchable_id: context[:issue].id,
-                                    user_id: issue.assigned_to_id)
-        end
+
+      issue = context[:issue]
+  		journal = context[:journal]
+
+      if issue.assigned_to_id != nil
+        Watcher.find_or_create_by(watchable_type: "Issue",
+                                  watchable_id: issue.id,
+                                  user_id: issue.assigned_to_id)
       end
+
+      Watcher.find_or_create_by(watchable_type: "Issue",
+                                watchable_id: issue.id,
+                                user_id: journal.user.id)
+
     end
-    
+
   end
 end
